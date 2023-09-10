2df4cb31725711d34fd2d70f4dea578472c4a79c
storycraft <storycraft@pancake.sh>
2023-09-10 21:59:11 +0900

    feat: fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@
  * Copyright (c) storycraft. Licensed under the MIT Licence.
  */
 
-/// `loco-protocol`: Rust Loco protocol implementation
-/// This crate provides low brick for building client on it
-///
-/// ## Specification
-#[doc = include_str!("../specification.md")]
+//! `loco-protocol`: Rust Loco protocol implementation
+//! This crate provides low brick for building client on it
+//!
+//! ## Specification
+#![doc = include_str!("../specification.md")]
 
-/// Loco protocol implementation
+// Loco protocol implementation
 pub mod command;
 
 /// Secure loyer implementation
